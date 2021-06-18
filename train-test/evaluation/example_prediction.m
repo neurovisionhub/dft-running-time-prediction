0247b895ac360bc8bca663f22a1d21b740f8af67
ORD <ceo@neurovision.ai>
2021-06-18 01:04:18 -0400

    commit

--- train-test/evaluation/example_prediction.m
+++ train-test/evaluation/example_prediction.m
@@ -1,4 +1,4 @@
-function [test_trace] = example_prediction(data_web_in,p_test,NHNeurons)
+function [test_trace,str,str_a] = example_prediction(data_web_in,p_test,NHNeurons)
 test_trace=[];
 %%  ----------- Test ----------------------- %%
 %% baseline / DFT runtime bp prediction K=1000 %%
@@ -7,6 +7,29 @@ test_trace=[];
 %% 4: BM-WAND/ClueWeb/BM25, 5: BM-WAND/Gov2/BM25
 %% 6: BM-WAND/ClueWeb/TfIDF: 7: BM-WAND/Gov2/TfIDF %%
 data_web = data_web_in;
+str = '';
+switch data_web
+      case 0
+        str ='W.CW.BM25'; 
+      case 1
+        str ='W.Gov2.BM25'; 
+      case 2
+        str ='W.CW.TfIDF'; 
+      case 3
+        str ='W.Gov2.TfIDF'; 
+      case 4
+        str ='BMW.CW.BM25'; 
+      case 5
+        str ='BMW.Gov2.BM25'; 
+      case 6
+        str ='BMW.CW.TfIDF'; 
+      case 7
+        str ='BMW.Gov2.TfIDF';   
+      
+      otherwise
+        disp('no menu')
+end
+
 %% Partition train-test data
 % Data for Test (ie. 40% => 60% train + 40% test)
 %percentTest = 0.4;
@@ -19,5 +42,7 @@ percentTest = p_test;
 %% DFT-prediction
 [~,TestingAccuracy,rOut] = simple_BP_test_dft_prediction(data_web,percentTest,NumberofHiddenNeurons);
 [test_trace] = [test_trace;rOut,TestingAccuracy];
+str_a = ["PCr","RMSE"];
+str = [string(str),string(str)];
 %%  --------------------------------------------- %%
 end
